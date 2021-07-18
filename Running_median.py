df036a25cbb1723059e850ea5fddd3d7897a83a3
prodigy803 <pushkaraj803@gmail.com>
2021-07-19 01:22:12 +0530

    Will attempt Running Median Problem later

---
+++ Running_median.py
@@ -0,0 +1,9 @@
+"""
+Given a string s which is sort of streaming -> 's' -> 'si' -> 'sil' -> 'sile' -> 'silen'
+find the current median of the string.
+"""
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        
+        return None
